63576a1aec0e0a5f262983c5be9b8838e9250e42
Shahin Azad <me@5hah.in>
2015-03-28 11:38:07 +0430

    Remove mistake contribution.

--- flowdock_toggle_direction.user.js
+++ flowdock_toggle_direction.user.js
@@ -1,3 +1,10 @@
+// ==UserScript==
+// @name        Flowdock toggle direction
+// @namespace   Flowdock
+// @include     https://www.flowdock.com
+// @version     1
+// @grant       none
+// ==/UserScript==
 $('.msg-body').dblclick(function(){
     var current_dir = $(this).parent().css('direction');
     if(current_dir == undefined || current_dir == 'ltr'){
