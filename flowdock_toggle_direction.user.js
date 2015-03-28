e3d96925a80c5ef10f535009db850b570a66b23d
Shahin Azad <me@5hah.in>
2015-03-28 12:01:59 +0430

    Apply a wildcard to effect all pages.

--- flowdock_toggle_direction.user.js
+++ flowdock_toggle_direction.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name        Flowdock toggle direction
 // @namespace   Flowdock
-// @include     https://www.flowdock.com
+// @include     https://www.flowdock.com/*
 // @version     1
 // @grant       none
 // ==/UserScript==
-$('.msg-body').dblclick(function(){
-    var current_dir = $(this).parent().css('direction');
-    if(current_dir == undefined || current_dir == 'ltr'){
-	$(this).parent().css({direction: 'rtl'});
-    } else {
-	$(this).parent().css({direction: 'ltr'});
-    }
+$(document).ready(function(){
+    $('.msg-body').dblclick(function(){
+	var current_dir = $(this).parent().css('direction');
+	if(current_dir == undefined || current_dir == 'ltr'){
+	    $(this).parent().css({direction: 'rtl'});
+	} else {
+	    $(this).parent().css({direction: 'ltr'});
+	}
+    });
 });
